41e71754c082e7c4236d8c66df6e169771cc7fce
yuichiro-takeda <yuichiro-takeda@bm-sms.co.jp>
2017-08-01 17:56:23 +0900

    インストーラーにzsh関連ファイルを追加

--- installer.sh
+++ installer.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
  
-ln -snvf "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
+ln -snvf "${HOME}/dotfiles/.zsh/.zshrc" "${HOME}/.zshrc"
+ln -snvf "${HOME}/dotfiles/.zsh/.zshrc.pre-oh-my-zsh" "${HOME}/.zshrc.pre-oh-my-zsh"
+ln -snvf "${HOME}/dotfiles/.oh-my-zsh" "${HOME}/.oh-my-zsh"
