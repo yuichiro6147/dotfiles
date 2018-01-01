ffd3d609b01d70db76af38fa7dc381b1889b729c
Yuichiro Takeda <y-takeda.6147@i.softbank.jp>
2018-01-01 18:33:17 +0900

    installerにzshとoh-my-zshを追加

--- installer.sh
+++ installer.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# zshのinstall
+brew install zsh
+
+# oh-my-zshのinstall
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
  
 ln -snvf "${HOME}/dotfiles/.zsh/.zshrc" "${HOME}/.zshrc"
 ln -snvf "${HOME}/dotfiles/.zsh/.zshrc.pre-oh-my-zsh" "${HOME}/.zshrc.pre-oh-my-zsh"
