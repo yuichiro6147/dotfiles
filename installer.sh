8433ffeb5ba3f6f31bd56c28a352bb6c970fc7c4
Yuichiro Takeda <tak6147@gmail.com>
2018-07-24 17:43:15 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,3 +9,13 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 ln -snvf "${HOME}/dotfiles/.zsh/.zshrc" "${HOME}/.zshrc"
 ln -snvf "${HOME}/dotfiles/.zsh/.zshrc.pre-oh-my-zsh" "${HOME}/.zshrc.pre-oh-my-zsh"
 ln -snvf "${HOME}/dotfiles/.oh-my-zsh" "${HOME}/.oh-my-zsh"
+
+# pecoのインストール
+brew install peco
+
+# anyframeの導入
+mkdir -p $HOME/.zsh/
+cd $HOME/.zsh/
+git clone https://github.com/mollifier/anyframe.git
+
+source $HOME/.zshrc
